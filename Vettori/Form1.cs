f5f6ccc0bcd4d0b0f54d3d53bbd155a82f7c4887
Umberto Ballestrazzi <u.ballestrazzi@gmail.com>
2023-10-06 14:01:05 +0200

    Add project files.

---
+++ Vettori/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vettori
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Vector v = new Vector(5, 4);
+            txtVettore.Text = string.Format("{0}" , v);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Vector v = Vector.Parse(txtVettore.Text);
+            MessageBox.Show(v.ToString());
+        }
+    }
+}
---
+++ Vettori/Vector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vettori
+{
+    class Vector
+    {
+        private readonly double _x;
+        private readonly double _y;
+
+        public Vector(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public double X
+        {
+            get { return _x; }
+        }
+
+        public double Y => _y;
+
+
+        public Vector Add(Vector other)
+        {
+            return new Vector(X + other.X, Y + other.Y);
+        }
+
+        public static Vector Parse(string s)
+        {
+            if (s[0] != '(')
+                throw new Exception("Il vettore è sbagliato");
+
+            string s2 = s.Substring(1, s.Length - 2);
+            string[] parts = s2.Split(';');
+            return new Vector(double.Parse(parts[0]), double.Parse(parts[1]));
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0};{1})", X, Y);
+        }
+    }
+}
